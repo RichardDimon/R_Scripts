147a8892b1fedf1a968701465fc769646b08e654
RichardDimon <97484698+RichardDimon@users.noreply.github.com>
2025-11-28 14:54:53 +1100

    Update individual_kinship_by_pop_sp.R

--- individual_kinship_by_pop_sp.R
+++ individual_kinship_by_pop_sp.R
@@ -1,15 +1,16 @@
 ### Modified function to run only kinships within each pop AND within each sp within each pop
 ### i.e., accounts for different species that have the exact same site name
 
-individual_kinship_by_pop_sp <- function(dart_data, basedir, species, dataset, pop, sp, maf=0.05, mis=0.2, as_bigmat=TRUE) {
+individual_kinship_by_pop_sp <- function(dart_data, basedir, species, dataset, pop, sp, maf=0.1, mis=0.2, as_bigmat=TRUE) {
   require(SNPRelate)
-
-  # combine species + population as group ID
+  
+  # create species-pop ID
   spop <- paste(sp, pop, sep="__")
   spop_levels <- unique(spop)
   
   kinlist <- list()
   nsamp <- nrow(dart_data$gt)
+  
   igds_file <- dart2gds(dart_data, basedir, species, dataset)
   igds <- snpgdsOpen(igds_file)
   
@@ -18,46 +19,43 @@ individual_kinship_by_pop_sp <- function(dart_data, basedir, species, dataset, p
     
     iout <- snpgdsIBDMoM(
       igds,
-      sample.id=rownames(dart_data$gt)[isamps],
-      maf=maf,
-      missing.rate=mis,
-      num.thread=1,
-      kinship=TRUE
+      sample.id = rownames(dart_data$gt)[isamps],
+      maf = maf,
+      missing.rate = mis,
+      num.thread = 1,
+      kinship = TRUE
     )
     
     ikout <- iout$kinship
     rownames(ikout) <- rownames(dart_data$gt)[isamps]
     colnames(ikout) <- rownames(dart_data$gt)[isamps]
-    kinlist[[ grp ]] <- ikout
+    
+    kinlist[[grp]] <- ikout
   }
   
   snpgdsClose(igds)
   
-  if (as_bigmat) {
-    bigmat <- matrix(0, nsamp, nsamp)
-    rownames(bigmat) <- ""
-    colnames(bigmat) <- ""
+  if (!as_bigmat) return(kinlist)
+  
+  # ----------------------------------
+  # big matrix assembly by index mapping
+  # ----------------------------------
+  
+  bigmat <- matrix(0, nsamp, nsamp)
+  rownames(bigmat) <- rownames(dart_data$gt)
+  colnames(bigmat) <- rownames(dart_data$gt)
+  
+  for (grp in names(kinlist)) {
+    im <- kinlist[[grp]]
+    inds <- rownames(im)
     
-    istart <- 1
-    for (i in seq_along(kinlist)) {
-      im <- kinlist[[i]]
-      iN <- nrow(im)
-      
-      if (istart == 1) {
-        istop <- iN
-      } else {
-        istop <- istop + iN
-      }
-      
-      bigmat[istart:istop, istart:istop] <- im
-      rownames(bigmat)[istart:istop] <- rownames(im)
-      colnames(bigmat)[istart:istop] <- rownames(im)
-      istart <- istart + iN
-    }
+    # find positions in full sample order
+    idx <- match(inds, rownames(bigmat))
     
-    return(bigmat)
+    # copy sub-matrix
+    bigmat[idx, idx] <- im
   }
   
-  return(kinlist)
+  return(bigmat)
 }
 
