02c3ec443330404ea6c7b6dee068e5bfd97b3dff
RichardDimon <97484698+RichardDimon@users.noreply.github.com>
2025-11-28 14:30:03 +1100

    Add files via upload

---
+++ individual_kinship_by_pop_sp.R
@@ -0,0 +1,59 @@
+individual_kinship_by_pop_sp <- function(dart_data, basedir, species, dataset, pop, sp, maf=0.05, mis=0.2, as_bigmat=TRUE) {
+  require(SNPRelate)
+
+  # combine species + population as group ID
+  spop <- paste(sp, pop, sep="__")
+  spop_levels <- unique(spop)
+  
+  kinlist <- list()
+  nsamp <- nrow(dart_data$gt)
+  igds_file <- dart2gds(dart_data, basedir, species, dataset)
+  igds <- snpgdsOpen(igds_file)
+  
+  for (grp in spop_levels) {
+    isamps <- which(spop == grp)
+    
+    iout <- snpgdsIBDMoM(
+      igds,
+      sample.id=rownames(dart_data$gt)[isamps],
+      maf=maf,
+      missing.rate=mis,
+      num.thread=1,
+      kinship=TRUE
+    )
+    
+    ikout <- iout$kinship
+    rownames(ikout) <- rownames(dart_data$gt)[isamps]
+    colnames(ikout) <- rownames(dart_data$gt)[isamps]
+    kinlist[[ grp ]] <- ikout
+  }
+  
+  snpgdsClose(igds)
+  
+  if (as_bigmat) {
+    bigmat <- matrix(0, nsamp, nsamp)
+    rownames(bigmat) <- ""
+    colnames(bigmat) <- ""
+    
+    istart <- 1
+    for (i in seq_along(kinlist)) {
+      im <- kinlist[[i]]
+      iN <- nrow(im)
+      
+      if (istart == 1) {
+        istop <- iN
+      } else {
+        istop <- istop + iN
+      }
+      
+      bigmat[istart:istop, istart:istop] <- im
+      rownames(bigmat)[istart:istop] <- rownames(im)
+      colnames(bigmat)[istart:istop] <- rownames(im)
+      istart <- istart + iN
+    }
+    
+    return(bigmat)
+  }
+  
+  return(kinlist)
+}
